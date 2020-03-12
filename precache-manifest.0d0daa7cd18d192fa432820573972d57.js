7d815b76d59dd3ee77538197636959ee7ccf316d
Saravanan <sarvan.rb@gmail.com>
2020-03-12 15:19:59 +0530

    Updates

--- precache-manifest.0d0daa7cd18d192fa432820573972d57.js
+++ precache-manifest.0d0daa7cd18d192fa432820573972d57.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7877fdfdd63d16c9c42b67d78b713579",
+    "revision": "2b091ed4ebdd86bd9a0d0926fdf6d985",
     "url": "/react-login/index.html"
   },
   {
-    "revision": "441cf1a64ec27c13a54c",
+    "revision": "8c0a23eb57c602357000",
     "url": "/react-login/static/css/main.5f361e03.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-login/static/js/2.bdbbff7e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "441cf1a64ec27c13a54c",
-    "url": "/react-login/static/js/main.c1ac76df.chunk.js"
+    "revision": "8c0a23eb57c602357000",
+    "url": "/react-login/static/js/main.cd95f631.chunk.js"
   },
   {
     "revision": "8a22fc9a1d05799dceb0",
